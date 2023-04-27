439a89736946e9ca5ea1bdeddcc49cb5b5cab56b
Huy T. Vo <hvo@users.noreply.github.com>
2023-04-27 01:02:34 -0400

    Update spatial_join.py

--- spatial_join.py
+++ spatial_join.py
@@ -80,5 +80,5 @@ if __name__=='__main__':
     counts = rdd.mapPartitionsWithIndex(processTrips) \
         .reduceByKey(lambda x,y: x+y) \
         .collect()
-    print(sorted(counts, key=lambda x: -x[1]))
+    print('\n'.join(map(repr,sorted(counts, key=lambda x: -x[1]))))
 
